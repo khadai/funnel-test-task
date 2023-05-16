c0c470b6841119769c1a93090ca2ba99ea93d382
khadai <mykhailo.khadai@gmail.com>
2023-05-16 16:05:38 +0300

    Initial Commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react';
+import type { AppProps } from 'next/app';
+import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import theme from '@/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <MuiThemeProvider theme={theme}>
+            <Component {...pageProps} />
+            <CssBaseline />
+        </MuiThemeProvider>
+    );
 }
